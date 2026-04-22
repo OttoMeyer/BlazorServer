3fcc6930aa1913bad5b6a751eb3a1d67f71edb7b
OttoMear <zgmgtvz@gmail.com>
2026-04-22 16:59:07 +0800

    Start

---
+++ BlazorTestApp/DataTypes/UserDataTypes.cs
@@ -0,0 +1,8 @@
+namespace BlazorTestApp.UserDataTypes;
+
+public class UserInformation{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public DateTime LastLogin { get; set; }
+    public string? Status { get; set; }
+};
