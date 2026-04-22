65cadc56639bfe6e8c88b22256b639c2bcd82da0
OttoMear <zgmgtvz@gmail.com>
2026-04-22 23:34:36 +0800

    Some polish for login page

--- BlazorTestApp/obj/Debug/net10.0/BlazorTestApp.AssemblyInfo.cs
+++ BlazorTestApp/obj/Debug/net10.0/BlazorTestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59cd6914058928f9d48ad8020c7320d488b5e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80dd7eb8b2dad841d4ddc5d80e922dfc4b764924")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
